4276103386516280dba2579b7d9d871124eb217d
Viktoriia <pecourses>
2022-07-02 17:52:58 +0300

    Change db configs

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: process.env.DB_USER ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? 'admin',
+    database: process.env.DB_NAME ?? 'users_db_dev',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
