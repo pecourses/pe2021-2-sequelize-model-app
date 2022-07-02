98d421ca9c0b1050b4587564ac1dc5e101610810
Viktoriia <pecourses>
2022-07-02 18:21:04 +0300

    Add static path + middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { STATIC_PATH } = require('./constants');
 const { errorHandlers } = require('./middleware');
 const router = require('./router');
 
 const app = express();
+
+// встроенный миддлвар для раздачи статических файлов
+app.use(express.static(STATIC_PATH));
 
 app.use(
   express.json({
--- constants.js
+++ constants.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 const CONSTANTS = {
   USER_GENDER_LIST: ['male', 'female', 'other'],
+  STATIC_PATH: path.join(__dirname, process.env.STATIC_FOLDER),
 };
 
 module.exports = CONSTANTS;
