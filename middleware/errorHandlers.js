c58ecaa230a3b320e22113bcfaba69663ab4b2d1
Viktoriia <pecourses>
2022-07-02 16:40:41 +0300

    Add db & default error handlers

---
+++ middleware/errorHandlers.js
@@ -0,0 +1,33 @@
+const createError = require('http-errors');
+const {
+  Sequelize: { BaseError, ValidationError },
+} = require('./../models');
+
+module.exports.dbErrorHandler = (err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    const errors = err.errors.map(e => ({ status: 422, title: e.message }));
+
+    return res.status(422).send({ errors });
+  }
+  if (err instanceof BaseError) {
+    next(createError(500, 'Database Error'));
+  }
+
+  next(err);
+};
+
+module.exports.errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return;
+  }
+  console.log('err', err);
+  const errorStatus = err?.status ?? 500;
+  res.status(errorStatus).send({
+    errors: [
+      {
+        status: errorStatus,
+        title: err?.message ?? 'Internal Server Error',
+      },
+    ],
+  });
+};
